804b8c8d63c5b51543824d7f8e57861d17a29814
Steve Hambling <hambling8@gmail.com>
2025-02-21 07:41:15 -0500

    fixed cubic formulat to more standard form

--- extras/test_regression.cpp
+++ extras/test_regression.cpp
@@ -10,39 +10,41 @@ Testing 3rd order vs 4th order polynomial: No noticeable difference.
 #include <iomanip>
 
 using namespace std;
-const float A = 2.332225E+02;
-const float B = -9.784173E-02;
-const float C = 2.401275E-05;
-const float D = -3.491355E-09;
+const float A = -3.491355E-09;  // Ax^3
+const float B = 2.401275E-05;   // Bx^2
+const float C = -9.784173E-02;  // Cx
+const float D = 2.332225E+02;   // D
 
-const float qA = 2.9476119E+02;
-const float qB = -2.0544626E-01;
-const float qC = 9.3818106E-05;
-const float qD = -2.3412916E-08;
-const float qE = 2.1114968E-12;
+const float qA = 2.1114968E-12;  // Ax^4
+const float qB = -2.3412916E-08; // Bx^3
+const float qC = 9.3818106E-05;  // Cx^2
+const float qD = -2.0544626E-01; // Dx
+const float qE = 2.9476119E+02;  // E
 
-// const double A = 2.33222507502202E+02;
-// const double B = -9.78417339763722E-02;
-// const double C = 2.40127508342357E-05;
-// const double D = -3.49135518682689E-09;
 
-// const double qA = 2.94761189325819E+02;
-// const double qB = -2.05446256486982E-01;
-// const double qC = 9.38181058287797E-05;
-// const double qD = -2.3412916496839E-08;
-// const double qE = 2.1114967506098E-12;
 
-// 3rd Order Polynomial (Cubic Formula) TempF = A+Bx+C*x^2+D*x^3
+// const double A = -3.491355E-09;  // Ax^3
+// const double B = 2.401275E-05;   // Bx^2
+// const double C = -9.784173E-02;  // Cx
+// const double D = 2.332225E+02;   // D
+
+// const double qA = 2.1114968E-12;  // Ax^4
+// const double qB = -2.3412916E-08; // Bx^3
+// const double qC = 9.3818106E-05;  // Cx^2
+// const double qD = -2.0544626E-01; // Dx
+// const double qE = 2.9476119E+02;  // E
+
+// 3rd Order Polynomial (Cubic Formula) TempF = Ax^3+Bx^2+Cx+D
 float getCubicTemp(int ADC_value) {
     int x = ADC_value;
-    float tempF = A+B*x+C*pow(x, 2)+D*pow(x, 3);
+    float tempF = A*pow(x, 3)+B*pow(x, 2)+C*x+D;
     return tempF;
 }
 
-// 4th Order Polynomial (Quartic Formula) TempF = A+Bx+C*x^2+D*x^3+E*x^4
+// 4th Order Polynomial (Quartic Formula) TempF = Ax^4+Bx^3+Cx^2+Dx+E
 float getQuarticTemp(int ADC_value) {
     int x = ADC_value;
-    float tempF = qA+qB*x+qC*pow(x, 2)+qD*pow(x, 3)+qE*pow(x, 4);
+    float tempF = tempF = qA*pow(x, 4)+qB*pow(x, 3)+qC*pow(x, 2)+qD*x+qE;
     return tempF;
 }
 
